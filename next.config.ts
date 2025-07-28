7322c921495766c03777a4aa9debae6f7c90e967
Xeno <admin@MacBook-Pro.local>
2025-07-28 13:56:22 +0700

    Fix Type error: Property 'role' does not exist on type

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,19 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   allowedDevOrigins: ['http://192.168.1.57:3000'],
+  webpack: (config, { isServer }) => {
+    config.externals = config.externals || []
+    config.resolve.fallback = {
+      ...config.resolve.fallback,
+      fs: false,
+      tls: false,
+      net: false,
+      dns: false,
+      crypto: false,
+    }
+
+    return config
+  },
 };
 
 export default nextConfig;
