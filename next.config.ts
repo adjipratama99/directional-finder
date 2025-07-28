7c759ee1f6498d57ad18d69243dddfd0862dbd6b
Xeno <admin@MacBook-Pro.local>
2025-07-28 14:00:02 +0700

    Fix error module on typeorm

--- next.config.ts
+++ next.config.ts
@@ -5,6 +5,10 @@ const nextConfig: NextConfig = {
   allowedDevOrigins: ['http://192.168.1.57:3000'],
   webpack: (config, { isServer }) => {
     config.externals = config.externals || []
+    config.externals.push({
+      'react-native-sqlite-storage': 'commonjs react-native-sqlite-storage',
+      '@sap/hana-client': 'commonjs @sap/hana-client',
+    })
     config.resolve.fallback = {
       ...config.resolve.fallback,
       fs: false,
