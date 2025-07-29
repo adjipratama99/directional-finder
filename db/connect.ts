bb13a0cba07be020cddfdffb9d3643a3c9f1692d
Xeno <admin@MacBook-Pro.local>
2025-07-29 14:32:42 +0700

    Migrating to Supabase

--- db/connect.ts
+++ db/connect.ts
@@ -9,16 +9,18 @@ const __dirname = path.dirname(__filename);
 
 const cert = fs.readFileSync(path.resolve(__dirname, './prod-ca-2021.crt')).toString();
 
-if (!process.env.SUPABASE_PUBLIC_URL) {
+if (!process.env.POSTGRES_URL_NON_POOLING) {
     throw new Error('Missing SUPABASE_PUBLIC_URL in environment variables');
 }
 
-export const sequelize = new Sequelize(process.env.SUPABASE_PUBLIC_URL, {
+export const sequelize = new Sequelize(process.env.POSTGRES_URL_NON_POOLING, {
     dialect: 'postgres',
     dialectModule: pg,
-    ssl: {
-        rejectUnauthorized: true,
-        cert
+    dialectOptions: {
+        ssl: {
+            rejectUnauthorized: true,
+            cert
+        },
     },
     logging: process.env.NODE_ENV === 'development' ? console.log : false,
 });
