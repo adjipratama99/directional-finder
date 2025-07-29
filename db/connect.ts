ce67507acc8f9c8e96664605e6b738fc65f089f8
Xeno <admin@MacBook-Pro.local>
2025-07-29 15:02:59 +0700

    update cert value

--- db/connect.ts
+++ db/connect.ts
@@ -1,13 +1,6 @@
 // db/connect.ts
 import { Sequelize } from 'sequelize';
 import pg from 'pg';
-import fs from 'fs';
-import path from 'path';
-import { fileURLToPath } from "url";
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-
-const cert = fs.readFileSync(path.resolve(__dirname, './prod-ca-2021.crt')).toString();
 
 if (!process.env.POSTGRES_URL_NON_POOLING) {
     throw new Error('Missing SUPABASE_PUBLIC_URL in environment variables');
